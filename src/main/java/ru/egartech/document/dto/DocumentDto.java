55d00db0b897d8a35a2f42d02b0729cd9430c350
zxc <psychokid534@gmail.com>
2024-05-29 22:50:00 +0300

    Fix edit document

--- src/main/java/ru/egartech/document/dto/DocumentDto.java
+++ src/main/java/ru/egartech/document/dto/DocumentDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -27,6 +28,7 @@ public class DocumentDto {
 
     @JsonProperty("date")
     @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
 
     @JsonProperty("author")
